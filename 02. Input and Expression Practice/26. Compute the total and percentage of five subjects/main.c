5492cb69ae2f54ee0bd14639a3d148d5033b8640
omkar prabhu <prabhuomkar068@gmail.com>
2026-07-28 20:25:47 +0530

    update file 26

--- 02. Input and Expression Practice/26. Compute the total and percentage of five subjects/main.c
+++ 02. Input and Expression Practice/26. Compute the total and percentage of five subjects/main.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
+    float s1, s2, s3, s4, s5;
+    // enter the marks out of 20
+    printf("enter the marks of the five subjects: ");
+    scanf("%f %f %f %f %f", &s1, &s2, &s3, &s4, &s5);
+
+    printf("your percentage is %.2f %.2f", (s1+s2+s3+s4+s5/100* 100),(s1+s2+s3+s4+s5));
     
     return 0;
 }
